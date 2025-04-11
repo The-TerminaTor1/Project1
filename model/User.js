fbf6db472506fb6adb1a7f6e2d42563cddf69c04
admin1 <theterminatororg@gmail.com>
2025-04-11 19:44:38 +0530

    Save local changes to User model

--- model/User.js
+++ model/User.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String },
   isVerified: { type: Boolean, default: false },
+  otpVerified: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('User', userSchema);
